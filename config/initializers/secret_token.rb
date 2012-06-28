64a16763813e462456cdb851a20791d622c11fc0
Charlie Sanders <sanderscharlie@gmail.com>
2012-06-27 19:24:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Krnlsndrs::Application.config.secret_token = '93b7ce581215fd0c47a5dbc1741b9694037b3bbcbf176ee96a5e991936817e93fe20ba5523f8a1fbb6d163a75e26f13da87a5228af813f9eb42bd08b88125c68'
